c1739a7c3d8602ca34246f2cf069939c86b4f34e
José <ribeirojoseph44@gmail.com>
2022-03-08 10:05:52 -0300

    Criando base de dados e tabela 'fornecedores'

---
+++ api/banco-de-dados/criarTabelas.js
@@ -0,0 +1,8 @@
+const ModeloTabela = require('../rotas/fornecedores/modeloTabelaFornecedor');
+
+ModeloTabela
+    .sync()
+    .then(() => {
+        console.log('Tabela criada com sucesso!')
+    })
+    .catch(console.log());
--- api/banco-de-dados/index.js
+++ api/banco-de-dados/index.js
@@ -1,24 +1,26 @@
 // Arquivo de credenciais de conexão do banco-de-dados
 
 const Sequelize = require('sequelize');
-// const config = require('config');
+const config = require('config');
 
 const instancia = new Sequelize(
-    // config.get('mysql.banco-de-dados'),
-    // config.get('mysql.usuario'),
-    // config.get('mysql.senha'),
-    // {
-    //     host: config.get('mysql.host'),
-    //     dialect: 'mysql'
-    // }
-
-    process.env.Usuario,
-    process.env.Senha,
-    process.env.Banco-de-dados,
+    config.get('mysql.banco-de-dados'),
+    config.get('mysql.usuario'),
+    config.get('mysql.senha'),
     {
-        host: process.env.Host,
-        dialect: 'mysql'
+        host: config.get('mysql.host'),
+        dialect: 'mysql',
+        port: config.get('mysql.port')
     }
+
+    // process.env.Banco_de_dados,
+    // process.env.Usuario,
+    // process.env.Senha,
+    // {
+    //     dialect: 'mysql',
+    //     host: process.env.Host,
+    //     port: process.env.port
+    // }
 );
 
 module.exports = instancia;
