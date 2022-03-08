bbddbeedf19bb5802554c7e3668715d19faae82e
José <ribeirojoseph44@gmail.com>
2022-03-08 14:34:40 -0300

    GET Fornecedores por ID

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -4,7 +4,21 @@ module.exports = {
     listar () {
         return Modelo.findAll();
     },
+
     inserir (fornecedor) {
         return Modelo.create(fornecedor);
+    },
+    
+    async pegarPorId (id) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        });
+        if (!encontrado) {
+            console.log('Fornecedor não encontrado!');
+            throw new Error('Fornecedor não encontrado!');
+        }
+        return encontrado;
     }
 }
