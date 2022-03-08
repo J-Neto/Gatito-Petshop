9741239fb1981bea480e4d74419a402af900695a
José <ribeirojoseph44@gmail.com>
2022-03-08 12:04:47 -0300

    Adicionando método POST

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -2,6 +2,9 @@ const Modelo = require('./modeloTabelaFornecedor');
 
 module.exports = {
     listar () {
-        return Modelo.findAll()
+        return Modelo.findAll();
+    },
+    inserir (fornecedor) {
+        return Modelo.create(fornecedor);
     }
 }
