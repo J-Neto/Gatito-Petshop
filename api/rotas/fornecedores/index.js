4155228c1fb14f8b7278efdf4043ed111755edce
José <ribeirojoseph44@gmail.com>
2022-03-09 11:50:27 -0300

    Adicionando status response aos métodos

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -7,6 +7,7 @@ const Fornecedor = require('./Fornecedor');
 // Listar fornecedores
 roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar();
+    res.status(200);
     res.send(JSON.stringify(resultados));
 });
 
@@ -16,8 +17,10 @@ roteador.post('/', async (req, res) => {
         const dadosRecebidos = req.body;
         const fornecedor = new Fornecedor(dadosRecebidos);
         await fornecedor.criar();
+        res.status(201);
         res.send(JSON.stringify(fornecedor))
     } catch (erro) {
+        res.status(400);
         res.send(JSON.stringify({
             mensagem: erro.message
         }))
@@ -37,6 +40,7 @@ roteador.get('/:idFornecedor', async (req, res) =>{
         // Respondendo ao usuário o fornecedor encontrado
         res.send(JSON.stringify(fornecedor));
     }catch(erro) {
+        res.status(404);
         res.send(JSON.stringify({
             mensagem: erro.message
         }))
@@ -59,10 +63,11 @@ roteador.put('/:idFornecedor', async (req, res) => {
         const fornecedor = new Fornecedor(dados);
 
         await fornecedor.atualizar();
-
+        res.status(204);
         res.end();
 
     } catch (erro) {
+        res.status(400);
         res.send(JSON.stringify(
             {message: erro.message}
         ))
@@ -76,8 +81,10 @@ roteador.delete('/:idFornecedor', async (req, res) => {
         const fornecedor = new Fornecedor({id: id});
         await fornecedor.carregar();
         await fornecedor.remover();
+        res.status(204);
         res.end();
     } catch (erro) {
+        res.status(404);
         res.send(
             JSON.stringify({
                 mensagem: erro.message
