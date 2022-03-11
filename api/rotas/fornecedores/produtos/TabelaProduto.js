714629c53ef86e42ca2c84e3b2730505adc4caaf
José <ribeirojoseph44@gmail.com>
2022-03-11 10:11:30 -0300

    Rota POST produtos

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -3,11 +3,16 @@
 const Modelo = require('./ModeloTabelaProduto');
 
 module.exports = {
+    // Pegando objetos a partir do id do fornecedor
     listar (idFornecedor) {
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
             }
         });
+    },
+
+    inserir (dados) {
+        return Modelo.create(dados);
     }
 }
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router({ mergeParams: true });
 const Tabela = require('./TabelaProduto');
+const Produto = require('./Produto');
 
 roteador.get('/', async (req, res) => {
     const produtos = await Tabela.listar(req.params.idFornecedor);
@@ -7,5 +8,17 @@ roteador.get('/', async (req, res) => {
         JSON.stringify(produtos)
     )
 });
+
+roteador.post('/', async (req, res) => {
+    const idFornecedor = req.params.idFornecedor;
+    const corpo = req.body;
+
+    // Unindo os dados recebidos
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
+    const produto = new Produto(dados);
+    await produto.criar();
+    res.status(201);
+    res.send(produto);
+})
 
 module.exports = roteador;
